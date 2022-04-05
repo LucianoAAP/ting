7654112259b2bfabfa26cda277faa5841e669eeb
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-04-05 01:30:11 -0300

    adiciona requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,34 @@
+import re
+from ting_file_management.file_management import txt_importer
+
+text_exists_word = [
+    {
+        "palavra": "pedro",
+        "arquivo": "statics/nome_pedro.txt",
+        "ocorrencias": [{"linha": 1}, {"linha": 3}],
+    }
+]
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    appearances = []
+    for file in instance.data:
+        appearance = {
+            'palavra': word,
+            'arquivo': file,
+            'ocorrencias': []
+        }
+        for index, line in enumerate(txt_importer(file)):
+            words = re.split(r'\W+', line)
+            lower_case_words = [line_word.lower() for line_word in words]
+            print(lower_case_words)
+            if word.lower() in lower_case_words:
+                appearance['ocorrencias'].append({'linha': index + 1})
+                print(appearance['ocorrencias'])
+        if len(appearance['ocorrencias']) > 0:
+            appearances.append(appearance)
+
+    return appearances
 
 
 def search_by_word(word, instance):
