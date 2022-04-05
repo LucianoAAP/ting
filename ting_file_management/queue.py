dcf07c17242edb9322d44050f0f4ddb6e6dfa075
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-04-04 21:56:49 -0300

    adiciona requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,28 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
+        self.length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
+        self.length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        first = self.data[0]
+        self.data = self.data[1:]
+        self.length -= 1
+        return first
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # try:
+        #     value = self.data[index]
+        #     return value
+        # except IndexError:
+        #     raise IndexError
+        if self.length == 0 or index < 0 or index >= self.length:
+            raise IndexError
+        else:
+            return self.data[index]
